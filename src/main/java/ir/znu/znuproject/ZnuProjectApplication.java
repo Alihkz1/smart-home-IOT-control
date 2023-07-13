ad692238a723401a1b76316bfb9e77260e4f7ca5
Ali Hosseinkhani <81155890+Alihkz1@users.noreply.github.com>
2023-07-13 16:37:49 -0700

    init project

---
+++ src/main/java/ir/znu/znuproject/ZnuProjectApplication.java
@@ -0,0 +1,13 @@
+package ir.znu.znuproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZnuProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZnuProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/znu/znuproject/ZnuProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.znu.znuproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZnuProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
