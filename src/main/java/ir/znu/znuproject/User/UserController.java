98e22435ebf2cfab0bc66d8dabb5ed65ba9e190e
Ali Hosseinkhani <81155890+Alihkz1@users.noreply.github.com>
2023-07-15 23:11:01 -0700

    Update UserController.java

--- src/main/java/ir/znu/znuproject/User/UserController.java
+++ src/main/java/ir/znu/znuproject/User/UserController.java
@@ -1,4 +1,28 @@
 package ir.znu.znuproject.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/user")
+@CrossOrigin(origins = "*")
 public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    List<User> getUserList() {
+        return userService.findAll();
+    }
+
+    @PostMapping
+    Boolean login(User user) {
+        return this.userService.login(user);
+    }
 }
