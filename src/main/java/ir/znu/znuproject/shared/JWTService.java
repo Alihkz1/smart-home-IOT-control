8e9c6970454b81e23d31323cf94e51e1d9b40cff
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2023-07-29 17:16:27 +0330

    Update JWTService.java

--- src/main/java/ir/znu/znuproject/shared/JWTService.java
+++ src/main/java/ir/znu/znuproject/shared/JWTService.java
@@ -51,7 +51,7 @@ public class JWTService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * (60 * 60 * 24)))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
