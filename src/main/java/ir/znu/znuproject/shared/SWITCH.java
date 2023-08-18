20caf85e9eda752311f294dd553ecd2dea2de87e
Ali Hosseinkhani <81155890+Alihkz1@users.noreply.github.com>
2023-08-18 14:41:24 -0700

    Update SWITCH.java

--- src/main/java/ir/znu/znuproject/shared/SWITCH.java
+++ src/main/java/ir/znu/znuproject/shared/SWITCH.java
@@ -1,6 +1,16 @@
 package ir.znu.znuproject.shared;
 
 public enum SWITCH {
-    ON,
-    OFF
+    ON("ON"),
+    OFF("OFF");
+
+    private String v;
+
+    SWITCH(String v) {
+        this.v = v;
+    }
+
+    public String getSWITCH() {
+        return v;
+    }
 }
