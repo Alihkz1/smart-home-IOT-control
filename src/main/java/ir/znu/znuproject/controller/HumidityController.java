b691dd29ad245482cfd7f9872cec4e5aa7d6e393
Ali Hosseinkhani <81155890+Alihkz1@users.noreply.github.com>
2023-08-27 20:21:07 -0700

    Update HumidityController.java

--- src/main/java/ir/znu/znuproject/controller/HumidityController.java
+++ src/main/java/ir/znu/znuproject/controller/HumidityController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("api/v1/humidity")
+@CrossOrigin(origins = "*")
 public class HumidityController {
 
     private final HumidityService humidityService;
